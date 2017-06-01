47cd081dc3ece146744c718bcadecd657bd7e8f9
Anthony Herbert <anthony@anthonyherbert.com>
2017-06-01 09:39:19 -0700

    8 - edit a member

--- app.py
+++ app.py
@@ -54,7 +54,20 @@ def add_member():
 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
 def edit_member(member_id):
-    return 'This updates a member by ID.'
+    new_member_data = request.get_json()
+
+    name = new_member_data['name']
+    email = new_member_data['email']
+    level = new_member_data['level']
+
+    db = get_db()
+    db.execute('update members set name = ?, email = ?, level = ? where id = ?', [name, email, level, member_id])
+    db.commit()
+
+    member_cur = db.execute('select id, name, email, level from members where id = ?', [member_id])
+    member = member_cur.fetchone()
+
+    return jsonify({'member' : {'id' : member['id'], 'name' : member['name'], 'email' : member['email'], 'level' : member['level']}})
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
